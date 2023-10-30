b6b0a64afe3622c7eb1abf23a299586412cc6232
mn0912 <monique.ngan@mail.utoronto.ca>
2023-10-30 12:46:12 -0400

    Added entity classes

---
+++ src/entity/ShoppingList.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+    private ArrayList<Ingredient> ingredients;
+
+    public ShoppingList(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public generateCost(){
+
+    }
+}
